4d9dc78935c685c8699fb2d46354132159d3d364
Ghanindra Bohora <bohoraghanindra@gmail.com>
2025-12-24 17:42:34 +0545

    Fix console log for API URL in api.js

--- Frontend/client/src/services/api.js
+++ Frontend/client/src/services/api.js
@@ -1,11 +1,11 @@
 // ======================= services/api.js =======================
 import axios from "axios";
-console.log("API URL before:", import.meta.env.VITE_API_URL); // should show URL
+
 
 const api = axios.create({
 baseURL: import.meta.env.VITE_API_URL,
 });
-// console.log("API URL:", import.meta.env.VITE_API_URL)
+console.log("API URL:", import.meta.env.VITE_API_URL)
 // alert(import.meta.env.VITE_API_URL);
 
 
